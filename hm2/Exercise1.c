fb2452c4ee5f50a06ed04c4641ab835ab3fdc4c1
grrigoryanalen10 <alen_grigoryan@edu.aua.am>
2025-03-14 22:48:45 +0400

    Update Exercise1.c

--- hm2/Exercise1.c
+++ hm2/Exercise1.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
 
-    FILE* fptr = fopen("file.txt", "r");
-    char ch = ' ';
-    int positionI = 1; // row position
-    int positionJ = 1; // col position
- 
+    // open the file "file.txt" for reading
+    FILE* filePointer = fopen("file.txt", "r");
+    
+    char character = ' ';
 
-    if (fptr == NULL) {
-        printf("error\n");
-        exit(1);
+    int row = 1; // row position (starting at 1)
+    int col = 1; // column position (starting at 1)
+
+    // check if the file was successfully opened
+    if (filePointer == NULL) {
+        printf("error\n"); 
+        return 0;
     }
 
-    while ((ch = fgetc(fptr)) != EOF) {
-        if (ch == '\n') {
-            positionI++;
-        } else {
-            positionJ++;
-            printf("Position: %d, %d, Char: %c\n", positionI, positionJ, ch);
+    // loop through each character in the file until reaching the end of the file (EOF)
+    while ((character = fgetc(filePointer)) != EOF) {
+        col++; // move to the next column as we read each character
+        if (character == '\n') {
+            row++; // move to the next row
+            col = 1; // reset column to 1 for the new row
+        }
+        // check if the character is a curly brace
+        else if (character == '{' || character == '}')
+            printf("'%c' at line %d, column %d\n", character, row, col);
         }
     }
 
-    fclose(fptr);
+    fclose(filePointer);
 
-    return 0;
+    return 0; 
 }
